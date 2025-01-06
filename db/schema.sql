cd2a64759482d07ca809a3a01c941d22595352cc
Ace <matt.aceves@yahoo.com>
2025-01-05 21:16:59 -0700

    fixed schema file

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE role (
 id SERIAL PRIMARY KEY,
 title VARCHAR(30) UNIQUE NOT NULL,
 salary DECIMAL NOT NULL,
-department_id INTEGER NOT NULL
+department_id INTEGER NOT NULL,
 FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
@@ -21,7 +21,7 @@ id SERIAL PRIMARY KEY,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 role_id INTEGER NOT NULL,
-manager_id INTEGER
-FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
+manager_id INTEGER,
+FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
 FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
